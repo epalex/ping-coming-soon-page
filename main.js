fc512fe0cdc830b852f13d2944bd026d7969c0e5
Epameinondas Alexopoulos <ep.alex@gmail.com>
2021-09-09 11:39:16 +0300

    desktop version is ready, js needs improvements

--- main.js
+++ main.js
@@ -12,5 +12,15 @@ submitButton.addEventListener("click", (e) => {
         const emailError = document.querySelector("#emailError");
         emailError.classList.add("visible");
         email.classList.add("invalid");
+    } else {
+        if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+            const emailError = document.querySelector("#emailError");
+            emailError.textContent = "Please provide a valid email address";
+            emailError.classList.add("visible");
+            email.classList.add("invalid");
+            email.value = "";
+            email.placeholder = "example@email/com";
+            console.log(email.placeholder);    
+        }
     }
 });
